b89ef47e6726dac2dde4ad57858bcfd229753ef2
Maru Khan <romirov@gmail.com>
2023-10-20 00:17:24 +0300

    changed getMinsSinceBeginningDay test

--- src/builtInTools/getMinutesPassedSinceBeginningDay.test.js
+++ src/builtInTools/getMinutesPassedSinceBeginningDay.test.js
@@ -3,7 +3,7 @@ import { getMinsSinceBeginningDay } from './getMinutesPassedSinceBeginningDay';
 describe('get minutes passed since of the beginning day', () => {
   test('get minutes passed since of the beginning day', () => {
     // eslint-disable-next-line new-cap, no-template-curly-in-string
-    Date.now = jest.fn().mockReturnValue(new Date('2023-10-19T23:59:00+03:00'));
+    Date.now = jest.fn().mockReturnValue(new Date('2023-10-19T23:59:00'));
     // eslint-disable-next-line no-console
     console.log = jest.fn();
     // eslint-disable-next-line new-cap
