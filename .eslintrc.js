a6151a23dc0934fb0a65f671bd2fce1a31568378
Maru Khan <romirov@gmail.com>
2023-11-09 23:17:00 +0300

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    'jest/globals': true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'max-len': [
+      'error',
+      {
+        ignoreComments: true,
+      },
+    ],
+    'import/prefer-default-export': 'off',
+  },
+};
