e7cb62bb189f1b4547bca826b1c56940cdec72ab
Maru Khan <romirov@gmail.com>
2023-11-16 00:20:56 +0300

    fix drawWeatherToCurrentLocation and half drawWeatherByInputField

--- scr/script.js
+++ scr/script.js
@@ -6,11 +6,10 @@ const numberOfStoredCities = 10;
  */
 async function getWeatherByCityName(cityName) {
   // eslint-disable-next-line max-len
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${openweathermapApiToken}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${openweathermapApiToken}&units=metric`;
   const response = await fetch(url);
   const data = await response.json();
   // eslint-disable-next-line no-console
-  console.log(data);
   return data;
 }
 
@@ -19,58 +18,28 @@ async function getWeatherByCityName(cityName) {
  */
 async function getWeatherByCoordinate(latitude, longitude) {
   // eslint-disable-next-line max-len
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${openweathermapApiToken}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${openweathermapApiToken}&units=metric`;
   const response = await fetch(url);
   const data = await response.json();
-  // eslint-disable-next-line no-console
-  console.log(data);
   return data;
-}
-
-/**
- * Получаем текущие координаты пользователя
- */
-// eslint-disable-next-line consistent-return
-function geoCurrentLocation() {
-  // eslint-disable-next-line no-new-object
-  const result = new Object();
-  result.latitude = Number.MAX_SAFE_INTEGER;
-  result.longitude = Number.MAX_SAFE_INTEGER;
-  if ("geolocation" in navigator) {
-    // eslint-disable-next-line no-console
-    console.log("местоположение доступно");
-    navigator.geolocation.getCurrentPosition((position) => {
-      result.latitude = position.coords.latitude;
-      result.longitude = position.coords.longitude;
-      // eslint-disable-next-line no-console, max-len
-      console.log(`Широта: ${result.latitude}, Долгота: ${result.longitude}`);
-      return result;
-    });
-  } else {
-    // eslint-disable-next-line no-console
-    console.log("Местоположение НЕ доступно");
-    return result;
-  }
 }
 
 /**
  * Содержит логику работы по выводу погоды
  */
-function drawWeather(data) {
-  const div = document.createElement("div");
-
+function drawWeather(el, data) {
   const title = document.createElement("h2");
-  title.innerText = data.name;
+  title.innerText = `Текущее местоположение: ${data.name}`;
 
   const temp = document.createElement("p");
-  temp.innerText = data.main.temp;
+  temp.innerText = `Температура: ${data.main.temp}°C`;
 
   const img = document.createElement("img");
   img.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
 
-  div.append(title);
-  div.append(temp);
-  div.append(img);
+  el.append(title);
+  el.append(temp);
+  el.append(img);
 }
 
 /**
@@ -90,14 +59,15 @@ function saveNewCityToLocalStorage(cityName, json) {
  * Обрабатывает ввод пользователя
  */
 function cityHandler() {
-  const cityName = document.getElementById("input").value;
-  const data = getWeatherByCityName(cityName);
-  // eslint-disable-next-line no-console
-  console.log(data);
-  // сохранение города в истории
-  saveNewCityToLocalStorage(cityName, JSON.stringify(data));
-  // вывод погоды
-  drawWeather(data);
+  const cityName = document.querySelector(".input").value;
+  getWeatherByCityName(cityName).then((data) => {
+    // eslint-disable-next-line no-console
+    console.log(data);
+    // сохранение города в истории
+    saveNewCityToLocalStorage(cityName, JSON.stringify(data));
+    // вывод погоды
+    drawWeather(document.querySelector(".inputCityWeather"), data);
+  });
 }
 
 /**
@@ -105,27 +75,51 @@ function cityHandler() {
  */
 function drawWeatherToCurrentLocation() {
   // погода (город, температура и иконка) в своей местности
-  const currentLocation = geoCurrentLocation();
-  // получение погоды по координатам
-  // eslint-disable-next-line max-len
-  const currentLocationWeather = getWeatherByCoordinate(
-    currentLocation.latitude,
-    currentLocation.longitude,
-  );
-  // вывод погоды
-  drawWeather(currentLocationWeather);
+  // eslint-disable-next-line no-new-object
+  if ("geolocation" in navigator) {
+    // eslint-disable-next-line no-console
+    console.log("местоположение доступно");
+    const currentLocation = {};
+    // получаем координаты текущего местоположения
+    navigator.geolocation.getCurrentPosition((position) => {
+      currentLocation.latitude = position.coords.latitude;
+      currentLocation.longitude = position.coords.longitude;
+      // eslint-disable-next-line no-console
+      console.log(`
+          Широта: ${currentLocation.latitude}, 
+          Долгота: ${currentLocation.longitude}
+        `);
+      // получаем погоду по координатам текущего местоположения
+      getWeatherByCoordinate(
+        currentLocation.latitude,
+        currentLocation.longitude,
+      ).then((currentLocationWeather) => {
+        // выводим погоду
+        // eslint-disable-next-line max-len
+        drawWeather(
+          document.querySelector(".currentLocationWeather"),
+          currentLocationWeather,
+        );
+      });
+    });
+  } else {
+    // eslint-disable-next-line no-console
+    console.log("местоположение НЕ доступно");
+  }
 }
 
 /**
  * Содержит логику работы по выводу погоды по запрошенному городу
  */
 function drawWeatherByInputField() {
-  const div = document.createElement("div");
+  const div = document.querySelector(".inputCityWeather");
   const inputField = document.createElement("input");
-  inputField.innerText = "enter your city";
+  inputField.defaultValue = "enter your city";
+  inputField.className = "input";
 
   const button = document.createElement("button");
   button.innerText = "Submit";
+
   button.addEventListener("click", cityHandler);
 
   div.append(inputField);
@@ -140,16 +134,16 @@ function cityHandlerLink(el) {
   const weather = getWeatherByCityName(cityName);
   // eslint-disable-next-line no-console
   console.log(weather);
-  drawWeather(weather);
+  drawWeather(document.querySelector(".inputCitiesWeather"), weather);
 }
 
 /**
- * Получает города из истории
+ * Содержит логику работы по выводу городов из истории
  */
 function drawCitiesFromLocalStorage() {
+  // получение городов из истории
   const cities = localStorage.Object.keys(localStorage);
-  const div = document.createElement("div");
-  div.className = "CitiesList";
+  const div = document.querySelector(".inputCitiesWeather");
   cities.array.forEach((element) => {
     const a = document.createElement("a");
     a.className = element;
@@ -175,49 +169,5 @@ function drawDataViaCreateElement() {
 
   drawCitiesFromLocalStorage();
 }
-// {
-//     "coord": {
-//         "lon": -0.1257,
-//         "lat": 51.5085
-//     },
-//     "weather": [
-//         {
-//             "id": 803,
-//             "main": "Clouds",
-//             "description": "broken clouds",
-//             "icon": "04n"
-//         }
-//     ],
-//     "base": "stations",
-//     "main": {
-//         "temp": 285.14,
-//         "feels_like": 284.51,
-//         "temp_min": 283.92,
-//         "temp_max": 286.36,
-//         "pressure": 999,
-//         "humidity": 81
-//     },
-//     "visibility": 10000,
-//     "wind": {
-//         "speed": 7.72,
-//         "deg": 240,
-//         "gust": 12.86
-//     },
-//     "clouds": {
-//         "all": 75
-//     },
-//     "dt": 1699910127,
-//     "sys": {
-//         "type": 2,
-//         "id": 2075535,
-//         "country": "GB",
-//         "sunrise": 1699859666,
-//         "sunset": 1699892111
-//     },
-//     "timezone": 0,
-//     "id": 2643743,
-//     "name": "London",
-//     "cod": 200
-// }
 
 drawDataViaCreateElement();
